3a79fc6f77990b544c7a56cee509a96fbc4f0ec9
Paweł Sienkiewicz <sienkol2001@gmail.com>
2023-12-08 14:11:23 +0100

    Test połączenia z bazą danych firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -43,6 +43,9 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/zadwyk/myapplication/MainActivity.kt
+++ app/src/main/java/com/zadwyk/myapplication/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.zadwyk.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var db: FirebaseFirestore
+    private lateinit var dataTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+        db = FirebaseFirestore.getInstance()
+        val docRef = db.collection("/ksiegarnie/ksiegarnia gdansk/książki")
+
+        dataTextView = findViewById(R.id.dataTextView)
+
+        docRef.get()
+            .addOnSuccessListener { documents ->
+                Log.d("Firestore", "Liczba dokumentów: ${documents.size()}") // Sprawdź liczbę dokumentów
+                val stringBuilder = StringBuilder()
+                for (document in documents) {
+                    val data = document.data
+                    for ((key, value) in data) {
+                        stringBuilder.append("$key: $value\n")
+                    }
+                    stringBuilder.append("\n") // Dodaj nową linię między dokumentami
+                }
+                dataTextView.text = stringBuilder.toString()
+            }
+            .addOnFailureListener { exception ->
+                Log.e("Firestore", "Błąd pobierania danych: $exception")
+            }
     }
 }
